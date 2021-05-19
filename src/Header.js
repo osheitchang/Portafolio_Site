62426ba22457edd54c7f5185a71507d92650bdc2
Osheit <chang.osheit@gmail.com>
2021-05-18 22:35:26 -0400

    icons

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Header.css'
 
+
 function Header() {
     return (
         <div>
-            
+            <h1> This will be the Header</h1>
         </div>
     )
 }
